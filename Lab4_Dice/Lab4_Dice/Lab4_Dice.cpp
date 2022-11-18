3b6dea9d485560f33c6820396e0a4e6b188154e9
Fortythird <evgeniikarmanovich@gmail.com>
2022-11-19 00:47:10 +0300

    Lab4 update
    Comments update

--- Lab4_Dice/Lab4_Dice/Lab4_Dice.cpp
+++ Lab4_Dice/Lab4_Dice/Lab4_Dice.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 int roll(string st)
 {
-    string data = "";
+    string data = "";                                                       // ѕеременна€ дл€ сборки данных о кубиках
     int num = 0, diceType = 0, mod = 1, sum = 0, pos = 0;
     bool hasMod = false;
-    while (pos < st.size())
+    while (pos < st.size())                                                 // ѕока переменна€ pos не будет больше длины строки, анализируем данные
     {
-        if (st[pos] == 'd')
-        {
+        if (st[pos] == 'd')                                                 // ≈сли встретилс€ символ d, то записываем количество 
+        {                                                                   // кубиков (записано до 'd') и очищаем data
             num = stoi(data);
             data = "";
         }
-        else if (st[pos] == '+' || st[pos] == '-')
-        {
-            hasMod = true;
+        else if (st[pos] == '+' || st[pos] == '-')                          // ≈сли встретилс€ знак модификатора, то записываем
+        {                                                                   // количество граней кубика, переключаем bool переменную в true,
+            hasMod = true;                                                  // что сигнализирует о наличии модификатора и очищаем data
             diceType = stoi(data);
             data = "";
-            if (st[pos] == '-') mod = -1;
+            if (st[pos] == '-') mod = -1;                                   // Ёто значение (1 или -1) далее будет множетелем к значению модификатора
         }
-        else if (st[pos] == ' ')
+        else if (st[pos] == ' ')                                            // ѕробел сигнализирует об окончании информации о кубиках одного типа
         {
             if (hasMod) mod *= stoi(data);
             else
@@ -29,9 +29,9 @@ int roll(string st)
                 mod = 0;
                 diceType = stoi(data);
             }
-            for (int i = 0; i < num; i++) sum += rand() % diceType + 1;
+            for (int i = 0; i < num; i++) sum += rand() % diceType + 1;     // Ѕроски кубиков указанного типа
             sum += mod;
-            hasMod = false;
+            hasMod = false;                                                 // —брос значений (по умолчанию)
             mod = 1;
             data = "";
             num = 0;
